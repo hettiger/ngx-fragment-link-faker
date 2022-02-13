6a0843ddb791140e804830b76eebbed66d4a2a1e
Martin Hettiger <martin@hettiger.com>
2022-02-13 20:55:46 +0100

    Implement basic demo application

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FragmentLinkFakerModule } from 'ngx-fragment-link-faker';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FragmentLinkFakerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
