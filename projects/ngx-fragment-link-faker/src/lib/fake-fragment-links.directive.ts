854002ede9b572fb242956f39d192678ce8922f2
Martin Hettiger <martin@hettiger.com>
2022-02-14 21:36:25 +0100

    Refactor directive

--- projects/ngx-fragment-link-faker/src/lib/fake-fragment-links.directive.ts
+++ projects/ngx-fragment-link-faker/src/lib/fake-fragment-links.directive.ts
@@ -21,50 +21,57 @@ export class FakeFragmentLinksDirective implements AfterViewInit, OnDestroy {
 
   private destroy$ = new Subject();
   private click$?: Observable<MouseEvent>;
+  private fragment$: Observable<string>;
 
   constructor(
     private elementRef: ElementRef,
     private router: Router,
-    private activatedRoute: ActivatedRoute,
     @Optional() @Inject(FAKE_FRAGMENT_LINKS_CONFIG) private config: FakeFragmentLinksConfig | null,
-  ) {}
+    activatedRoute: ActivatedRoute,
+  ) {
+    this.fragment$ = activatedRoute.fragment.pipe(
+      filter((fragment): fragment is string => !!fragment),
+      map(fragment => decodeURIComponent(fragment)),
+    )
+  }
 
   ngOnDestroy() {
     this.destroy$.next(true);
   }
 
   ngAfterViewInit() {
-    this.click$ = fromEvent(this.elementRef.nativeElement.querySelectorAll('a[href^="#"]'), 'click');
+    this.click$ = fromEvent(
+      this.elementRef.nativeElement.querySelectorAll('a[href^="#"]'), 'click'
+    );
+
     this.fakeAngularFragmentLinks();
-    this.scrollToFragmentTargetElements();
+    this.scrollOnFragmentUpdates();
   }
 
   private fakeAngularFragmentLinks() {
-    this.click$?.pipe(
-      takeUntil(this.destroy$),
-    )
-      .subscribe(
-        event => {
-          event.preventDefault();
-          const fragment = ((<HTMLAnchorElement | null>event.currentTarget)?.href.match(/#.*$/) ?? [''])[0].slice(1);
-          this.router.navigate([], { fragment, replaceUrl: true });
-        }
-      );
+    this.click$?.pipe(takeUntil(this.destroy$)).subscribe(
+      event => {
+        event.preventDefault();
+
+        const fragment = ((<HTMLAnchorElement | null>event.currentTarget)
+          ?.href.match(/#.*$/) ?? [''])[0].slice(1);
+
+        this.router.navigate([], { fragment, replaceUrl: true });
+      }
+    );
   }
 
-  private scrollToFragmentTargetElements() {
-    this.activatedRoute.fragment.pipe(
-      filter((fragment): fragment is string => !!fragment),
-      map(fragment => decodeURIComponent(fragment)),
+  private scrollOnFragmentUpdates() {
+    this.fragment$.pipe(
+      takeUntil(this.destroy$),
       map(fragment => document.querySelector(`[id="${fragment}"], [name="${fragment}"]`)),
-      filter((scrollTarget): scrollTarget is HTMLElement => !!scrollTarget)
-    )
-      .subscribe(
-        scrollTarget => window.scroll({
-          top: scrollTarget.offsetTop - this.scrollTopDelta,
-          behavior: this.scrollBehavior
-        })
-      );
+      filter((scrollTarget): scrollTarget is HTMLElement => scrollTarget instanceof HTMLElement)
+    ).subscribe(
+      scrollTarget => window.scroll({
+        top: scrollTarget.offsetTop - this.scrollTopDelta,
+        behavior: this.scrollBehavior
+      })
+    );
   }
 
 }
